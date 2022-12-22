c409aa14d5fabe773e9e205104a821576520e116
mask477 <mask477@gmail.com>
2022-12-23 01:40:52 +0500

    blogs page added with blod styling completed

--- blogs.php
+++ blogs.php
@@ -1,12 +1,179 @@
 <?php
   include('header.php');
 ?>
-<section>
+<section id="blogs-hero">
+  <div class="banner" style="background: url(/assets/images/blogs-banner.jpg) rgba(255, 255, 255, 0.3)"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="title text-center">News and Blogs</h1>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="blogs">
+  
+  <div class="container">
+    <div class="row">
+      <!-- Blog -->
+      <div class="col-lg-6 col-sm-12">
+        <div class="blog">
+          <div class="blog-thumnail-container">
+            <div class="blog-category">Interior Design</div>
+            <img src="/assets/images/blog1.jpg" alt="" width="100%" height="100%" class="blog-thumnail">
+          </div>
+          <!-- Blog Body -->
+          <div class="blog-body">
+            <h3 class="blog-title">Lorem ipsum dolor</h3>
+            <p class="blog-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam ratione explicabo sapiente! Amet earum aspernatur est tempore distinctio sequi. Minima consectetur mollitia.</p>
+            <!-- Blog Footer -->
+            <div class="blog-footer">
+              <!-- Author -->
+              <div class="blog-author">
+                <img src="https://i.pravatar.cc/250?img=5" alt="" class="blog-author-image" width="60" height="60">
+                <!-- Author Content -->
+                <div class="blog-author-content">
+                  <div class="blog-author-name">Anwar Pandaan</div>
+                  <div class="blog-author-date">March 16, 2022 . 6 min read</div>
+                </div>
+                <!-- End of Author Content -->
+              </div>
+              <!-- End of Author -->
+              <!-- Read More -->
+              <div class="blog-readmore">
+                Read more 
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+              </div>
+              <!-- End of Read More -->
+            </div>
+            <!-- End of Blog Footer -->
+          </div>
+          <!-- End of Blog Body -->
+        </div>
+      </div>
+      <!-- End of Blog -->
+      <!-- Blog -->
+      <div class="col-lg-6 col-sm-12">
+        <div class="blog">
+          <div class="blog-thumnail-container">
+            <div class="blog-category">Interior Design</div>
+            <img src="/assets/images/blog2.jpg" alt="" width="100%" height="100%" class="blog-thumnail">
+          </div>
+          <!-- Blog Body -->
+          <div class="blog-body">
+            <h3 class="blog-title">Lorem ipsum dolor</h3>
+            <p class="blog-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam ratione explicabo sapiente! Amet earum aspernatur est tempore distinctio sequi. Minima consectetur mollitia.</p>
+            <!-- Blog Footer -->
+            <div class="blog-footer">
+              <!-- Author -->
+              <div class="blog-author">
+                <img src="https://i.pravatar.cc/250?img=5" alt="" class="blog-author-image" width="60" height="60">
+                <!-- Author Content -->
+                <div class="blog-author-content">
+                  <div class="blog-author-name">Anwar Pandaan</div>
+                  <div class="blog-author-date">March 16, 2022 . 6 min read</div>
+                </div>
+                <!-- End of Author Content -->
+              </div>
+              <!-- End of Author -->
+              <!-- Read More -->
+              <div class="blog-readmore">
+                Read more 
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+              </div>
+              <!-- End of Read More -->
+            </div>
+            <!-- End of Blog Footer -->
+          </div>
+          <!-- End of Blog Body -->
+        </div>
+      </div>
+      <!-- End of Blog -->
+      <!-- Blog -->
+      <div class="col-lg-6 col-sm-12">
+        <div class="blog">
+          <div class="blog-thumnail-container">
+            <div class="blog-category">Interior Design</div>
+            <img src="/assets/images/blog3.jpg" alt="" width="100%" height="100%" class="blog-thumnail">
+          </div>
+          <!-- Blog Body -->
+          <div class="blog-body">
+            <h3 class="blog-title">Lorem ipsum dolor</h3>
+            <p class="blog-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam ratione explicabo sapiente! Amet earum aspernatur est tempore distinctio sequi. Minima consectetur mollitia.</p>
+            <!-- Blog Footer -->
+            <div class="blog-footer">
+              <!-- Author -->
+              <div class="blog-author">
+                <img src="https://i.pravatar.cc/250?img=5" alt="" class="blog-author-image" width="60" height="60">
+                <!-- Author Content -->
+                <div class="blog-author-content">
+                  <div class="blog-author-name">Anwar Pandaan</div>
+                  <div class="blog-author-date">March 16, 2022 . 6 min read</div>
+                </div>
+                <!-- End of Author Content -->
+              </div>
+              <!-- End of Author -->
+              <!-- Read More -->
+              <div class="blog-readmore">
+                Read more 
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+              </div>
+              <!-- End of Read More -->
+            </div>
+            <!-- End of Blog Footer -->
+          </div>
+          <!-- End of Blog Body -->
+        </div>
+      </div>
+      <!-- End of Blog -->
+      <!-- Blog -->
+      <div class="col-lg-6 col-sm-12">
+        <div class="blog">
+          <div class="blog-thumnail-container">
+            <div class="blog-category">Interior Design</div>
+            <img src="/assets/images/blog4.jpg" alt="" width="100%" height="100%" class="blog-thumnail">
+          </div>
+          <!-- Blog Body -->
+          <div class="blog-body">
+            <h3 class="blog-title">Lorem ipsum dolor</h3>
+            <p class="blog-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quisquam ratione explicabo sapiente! Amet earum aspernatur est tempore distinctio sequi. Minima consectetur mollitia.</p>
+            <!-- Blog Footer -->
+            <div class="blog-footer">
+              <!-- Author -->
+              <div class="blog-author">
+                <img src="https://i.pravatar.cc/250?img=5" alt="" class="blog-author-image" width="60" height="60">
+                <!-- Author Content -->
+                <div class="blog-author-content">
+                  <div class="blog-author-name">Anwar Pandaan</div>
+                  <div class="blog-author-date">March 16, 2022 . 6 min read</div>
+                </div>
+                <!-- End of Author Content -->
+              </div>
+              <!-- End of Author -->
+              <!-- Read More -->
+              <div class="blog-readmore">
+                Read more 
+                <ion-icon name="chevron-forward-outline"></ion-icon>
+              </div>
+              <!-- End of Read More -->
+            </div>
+            <!-- End of Blog Footer -->
+          </div>
+          <!-- End of Blog Body -->
+        </div>
+      </div>
+      <!-- End of Blog -->
+    </div>
+    <div class="row justify-content-center">
+      <button class="btn btn-light btn-rounded">Load More</button>
+    </div>
+  </div>
 
 </section>
 
 <!-- Change -->
-<section id="change">
+<section id="change" style="background: url(/assets/images/change-section-bg.jpg) rgba(255, 255, 255, 0.3);">
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-12 text-center">
